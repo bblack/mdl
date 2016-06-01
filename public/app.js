e7a871201e2834c553af06ad0da83217e419f0d3
Brian Black <black@gatech.edu>
2016-05-31 22:45:11 -0400

    slow, but working, texture mapping

--- public/app.js
+++ public/app.js
@@ -29,7 +29,10 @@ m.controller('QuadViewController', function($scope, $rootScope, $http){
         $rootScope.model = res.data;
         $rootScope.frame = 0;
     });
-
+    $http.get('palette')
+    .then(function(res){
+        $rootScope.palette = res.data;
+    });
     $scope.camPos = [0, -150, -100];
 });
 
@@ -292,11 +295,15 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
         template: '<canvas></canvas>',
         link: function($scope, $element){
             var zbuf;
+            var fbuf;
             function sizeCanvasToContainer(){
                 var w = $element.width();
                 var h = $element.height();
                 $element.find('canvas').attr('width', w).attr('height', h);
+                // giving 4 bytes per pixel is 4x as much space as is necessary--but it's
+                // worth it for a huge speedup in drawing it to the framebuffer
                 zbuf = new Uint8ClampedArray(4*w*h);
+                fbuf = new Uint8ClampedArray(4*w*h);
             }
             sizeCanvasToContainer();
             $(window).on('resize', sizeCanvasToContainer);
@@ -362,16 +369,30 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
             function signedParArea2(a, b, c){ // basically ||(b-a) x (c-b)||
                 return (c[0] - a[0])*(b[1] - a[1]) - (c[1] - a[1])*(b[0] - a[0]);
             }
-            function calcZAndWriteToZBufIfLower(w0, w1, w2, triarea2, screenVerts, w, y, x, zbuf){
-                w0 /= triarea2;
-                w1 /= triarea2;
-                w2 /= triarea2;
+            function calcZAndWriteToZBufIfLower(w0, w1, w2, triarea2, screenVerts, w, y, x, stVerts, facesFront, skin){
+                baryweights = [w0 / triarea2, w1 / triarea2, w2 / triarea2];
                 // interp p.z from vert z's
-                var z = w0*screenVerts[0][2] + w1*screenVerts[1][2] + w2*screenVerts[2][2];
+                var z = 0;
+                for (var i=0; i<3; i++) {
+                    z += baryweights[i]*screenVerts[i][2];
+                }
                 z *= 255;
                 var zbufindex = 4*(w * y + x);
+                var skinwidth = 296;
                 if (z < zbuf[zbufindex]) {
                     zbuf.fill(z, zbufindex, zbufindex + 3);
+                    var s = 0;
+                    var t = 0;
+                    for (var i=0; i<3; i++) {
+                        s += baryweights[i] * stVerts[i].s;
+                        if (!facesFront && stVerts[i].onSeam) {
+                            s += baryweights[i]*skinwidth/2;
+                        }
+                        t += baryweights[i] * stVerts[i].t;
+                    }
+                    var colorindex = skin[Math.floor(t)*skinwidth + Math.floor(s)];
+                    var color = $scope.$root.palette[colorindex];
+                    fbuf.set(color.concat(255), zbufindex);
                 }
             }
             function getbbox(sv){ // screenverts
@@ -382,7 +403,7 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
                     ymax: Math.ceil(Math.max(sv[0][1], sv[1][1], sv[2][1]))
                 };
             }
-            function rasterize(w, h, screenVerts){
+            function rasterize(w, h, screenVerts, stVerts, facesFront, skin){
                 var bbox = getbbox(screenVerts);
                 for (var x = Math.max(bbox.xmin, 0); x < bbox.xmax && x < w; x++) {
                     for (var y = Math.max(bbox.ymin, 0); y < bbox.ymax && y < h; y++) {
@@ -392,7 +413,8 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
                         var w1 = signedParArea2(screenVerts[2], screenVerts[0], p);
                         var w2 = signedParArea2(screenVerts[0], screenVerts[1], p);
                         if (w0 >= 0 && w1 >= 0 && w2 >= 0) { // p in screen tri?
-                            calcZAndWriteToZBufIfLower(w0, w1, w2, triarea2, screenVerts, w, y, x, zbuf);
+                            calcZAndWriteToZBufIfLower(w0, w1, w2, triarea2, screenVerts,
+                                w, y, x, stVerts, facesFront, skin);
                         }
                     }
                 }
@@ -401,6 +423,7 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
                 var start = Date.now();
                 var ctx = canvas.getContext('2d');
                 zbuf.fill(255);
+                fbuf.fill(0);
                 _.each(scene.entities, (e) => {
                     var frame = e.model.frames[$scope.frame].simpleFrame;
                     var objToWorldMatrix = [
@@ -410,18 +433,22 @@ m.directive('perspectiveProjectionRay', function(Vec3, $interval, MdlNorms){
                     ]
                     _.each(e.model.triangles, function rasterizeTri(tri){
                         var screenVerts = new Array(3);
-                        for (var i=0; i<tri.vertIndeces.length; i++) {
+                        var stVerts = new Array(3);
+                        for (var i=0; i<3; i++) {
                             var vertIndex = tri.vertIndeces[i];
                             var vert = frame.verts[vertIndex];
                             vert = new Vec3(vert.x, vert.y, vert.z);
                             vert = vert.applyAffineTransform(objToWorldMatrix);
                             screenVerts[i] = worldToCanvas(vert);
+                            stVerts[i] = e.model.texCoords[vertIndex];
                         }
-                        rasterize(canvas.width, canvas.height, screenVerts);
+                        rasterize(canvas.width, canvas.height, screenVerts, stVerts,
+                            tri.facesFront, e.model.skins[0].data.data);
                     })
                 })
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                ctx.putImageData(new ImageData(zbuf, canvas.width, canvas.height), 0, 0);
+                // fbuf.set(zbuf, 0);
+                ctx.putImageData(new ImageData(fbuf, canvas.width, canvas.height), 0, 0);
                 drawAxes(ctx);
                 recordRenderTime(Date.now() - start);
                 drawFps(ctx);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var Mdl = require('./lib/Mdl');
+var fs = require('fs');
 
 app.engine('html', require('ejs').renderFile);
 app.use(require('compression')());
@@ -14,6 +15,17 @@ app.get('/player.mdl', function(req, res){
     Mdl.readFile('./public/player.mdl', function(err, mdl){
         res.json(mdl);
     });
-});
+})
+.get('/palette', function(req, res){
+    var buf = fs.readFileSync('./public/palette.lmp');
+    var out = [];
+    for (var i=0; i<256; i++) {
+        var r = buf.readUInt8(i*3);
+        var g = buf.readUInt8(i*3 + 1);
+        var b = buf.readUInt8(i*3 + 2);
+        out.push([r, g, b]);
+    }
+    res.json(out);
+})
 
 app.listen(8088);
