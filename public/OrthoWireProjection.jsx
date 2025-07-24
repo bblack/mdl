a7d1f1ab9a857fe1732e01c4e6976db02371f9af
Brian Black <black@gatech.edu>
2025-07-24 09:49:30 -0400

    make cube show X on each face

--- public/OrthoWireProjection.jsx
+++ public/OrthoWireProjection.jsx
@@ -509,8 +509,8 @@ export default function OrthoWireProjection({mv, scene, tool, onToolSelected}) {
         const tris = [
           [2, 1, 0],
           [0, 3, 2],
-          [4, 5, 6],
-          [6, 7, 4],
+          [5, 6, 7],
+          [7, 4, 5],
           [1, 5, 4],
           [4, 0, 1],
           [6, 2, 3],
